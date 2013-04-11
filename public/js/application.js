a7d21f749ee7e7a4af77659cef80132f345517df
deeptianand <anandeepti@gmail.com>
2013-04-11 08:52:43 -0700

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,48 +1,40 @@
+function advance_player (player) {
+ var tr = $("#player"+player+"_strip");
+ var tds = tr.children().length
+ var td = $("#player"+player+"_strip").find('.active');
+ td.toggleClass('active');
+ var next = td.next()
+ next.toggleClass('active');
+ if(next.index() + 1 == tds){
+     //set the text on the winner element.winner element is still hidden
+ 	$('#winner').text("Yaaaaay!!!!  Player "+player+" is the winner...Starting next game")
+    //fade in the winner elment ..take about 5 seconds
+    $('#winner').fadeIn('5000')
+ 	//wait for 3 seconds before we restart the game, and hide the winner element
+ 	window.setTimeout(function (){restart_game(); $('#winner').hide()}, 3000);
+ }
+
+}
+
+function restart_game() {
+ var trs = $('tr')
+ 	$.each ( trs, function () {
+          var tds = $(this).find('td');
+	      $.each(tds,function () {
+		      $(this).removeClass('active')
+		      $(this).removeClass('winner')
+	      });
+	     $(this).find(">:first-child").addClass('active')
+      });
+}
+
 $(document).ready(function() {
-    var player1_counter = 1;
-    var player2_counter = 1;
-    var track_length = 40;
-
-    function get_winner(data) {
-      $.ajax ({
-        type: 'POST',
-        data: { sql: data },
-        url: '/results'
-      })
-    }
-
-  $(document).on('keyup', function(event) {
-
-    var code = event.keyCode;
-
-    function update_player_position(player) {
-      var active = $("#" + player + "_strip td.active");
-      active.removeClass('active').next().addClass('active');
-    }
-
-    if(code == 81) {
-      update_player_position('player1');
-      player1_counter += 1;
-      if ($('#player1_strip td.active').hasClass('winner')) {
-        $(document).off('keyup');
-        $('.p1_winner').css('display', 'inline');
-        var winner = $('.p1_winner').text();
-        get_winner(winner);
-        $('.results_button').css('display', 'block');
-      }
-    }
-
-    if(code == 80) {
-      update_player_position('player2');
-      player2_counter += 1;
-      if ($('#player2_strip td.active').hasClass('winner')) {
-        $(document).off('keyup');
-        $('.p2_winner').css('display', 'inline');
-        var winner = $('.p2_winner').text();
-        get_winner(winner);
-        $('.results_button').css('display', 'block');
-      }
-    }
-
-  });
+	$(document).on('keyup',function (event) {
+	  if(event.which == 81) {
+	  advance_player(1)
+	  }
+	  if(event.which ==80){
+	  	advance_player(2)  
+	  }
+});
 });
